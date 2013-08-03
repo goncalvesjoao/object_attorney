215f684a80b00a88def788ac89c619512d9690c6
goncalvesjoao <goncalves.joao@gmail.com>
2013-08-03 11:38:41 +0100

    split the nested_objects code onto another module

--- lib/object_attorney.rb
+++ lib/object_attorney.rb
@@ -1,4 +1,5 @@
 require "object_attorney/version"
+require "object_attorney/nested_objects"
 
 module ObjectAttorney
 
@@ -27,29 +28,12 @@ module ObjectAttorney
     mark_for_destruction_if_necessary(self, attributes)
   end
 
-  def nested_objects
-    unless nested_objects_updated
-      @nested_objects = self.class.instance_variable_get("@nested_objects").map { |nested_object_sym| self.send(nested_object_sym) }.flatten
-      nested_objects_updated = true
-    end
-    
-    @nested_objects
-  end
-
   def new_record?
     @object.try_or_return(:new_record?, true)
   end
 
   def persisted?
     @object.try_or_return(:persisted?, false)
-  end
-
-  def mark_for_destruction
-    @marked_for_destruction = true
-  end
-
-  def marked_for_destruction?
-    @marked_for_destruction
   end
 
   def save
@@ -72,14 +56,6 @@ module ObjectAttorney
     end
   end
 
-  def mark_for_destruction_if_necessary(object, attributes)
-    return nil unless attributes.kind_of?(Hash)
-
-    _destroy = attributes["_destroy"] || attributes[:_destroy]
-
-    object.mark_for_destruction if ["true", "1"].include?(_destroy)
-  end
-
   def read_attribute_for_serialization(attribute)
     respond_to?(attribute) ? send(attribute) : nil
   end
@@ -92,16 +68,16 @@ module ObjectAttorney
     base.class_eval do
       include ActiveModel::Validations
       include ActiveModel::Conversion
+      include ObjectAttorney::NestedObjects
 
-      attr_accessor :nested_objects_updated, :trusted_data
-      validate :validate_own_object, :validate_nested_objects
+      attr_accessor :trusted_data
+      validate :validate_own_object
 
       def valid?
         override_valid? ? true : super
       end
     end
 
-    base.instance_variable_set("@nested_objects", [])
     base.instance_variable_set("@white_list", [])
     base.instance_variable_set("@black_list", [])
   end
@@ -156,27 +132,10 @@ module ObjectAttorney
     @object.try_or_return(:save!, true)
   end
 
-  def save_or_destroy_nested_objects
-    result = nested_objects.map do |nested_object|
-      nested_object.marked_for_destruction? ? nested_object.destroy : nested_object.save!
-    end.all?
-
-    self.errors.add(:base, "Some errors where found while saving the nested objects.") unless result
-
-    result
-  end
-
   def validate_own_object
     valid = override_valid? ? true : @object.try_or_return(:valid?, true)
     import_own_object_errors unless valid
     valid
-  end
-
-  def validate_nested_objects
-    #nested_objects.all?(&:valid?) #will not validate all nested_objects
-    return true if nested_objects.reject(&:marked_for_destruction?).map(&:valid?).all?
-    import_nested_objects_errors
-    false
   end
 
   def get_attributes_for_existing(nested_object_name, existing_nested_object)
@@ -188,21 +147,6 @@ module ObjectAttorney
   def import_own_object_errors
     @object.errors.each { |key, value| self.errors.add(key, value) }
   end
-
-  def import_nested_objects_errors
-    self.class.instance_variable_get("@nested_objects").map do |nested_object_sym|
-      
-      [*self.send(nested_object_sym)].each do |nested_object|
-        nested_object.errors.full_messages.each { |message| self.errors.add(nested_object_sym, message) }
-      end
-
-    end
-  end
-
-  # def return_hash_with_existing_errors(hash, object = nil)
-  #   object ||= self
-  #   object.errors.empty? ? hash : hash.merge({ errors: object.errors })
-  # end
 
   private #------------------------------ private
 
@@ -220,48 +164,6 @@ module ObjectAttorney
     marked_for_destruction?
   end
 
-  def nested_getter(nested_object_name)
-    nested_instance_variable = self.instance_variable_get("@#{nested_object_name}")
-
-    if nested_instance_variable.nil?
-      nested_instance_variable = get_existing_and_new_nested_objects(nested_object_name)
-      self.instance_variable_set("@#{nested_object_name}", nested_instance_variable)
-    end
-
-    nested_instance_variable
-  end
-
-  def get_existing_and_new_nested_objects(nested_object_name)
-    existing_and_new_nested_objects = []
-    
-    update_existing_nested_objects(existing_and_new_nested_objects, nested_object_name)
-    build_new_nested_objects(existing_and_new_nested_objects, nested_object_name)
-
-    existing_and_new_nested_objects
-  end
-
-  def update_existing_nested_objects(existing_and_new_nested_objects, nested_object_name)
-    (send("existing_#{nested_object_name}") || []).each do |existing_nested_object|
-      attributes = get_attributes_for_existing(nested_object_name, existing_nested_object)
-
-      mark_for_destruction_if_necessary(existing_nested_object, attributes)
-      existing_nested_object.assign_attributes(attributes_without_destroy(attributes))
-
-      existing_and_new_nested_objects << existing_nested_object
-    end
-  end
-
-  def build_new_nested_objects(existing_and_new_nested_objects, nested_object_name)
-    (send("#{nested_object_name}_attributes") || {}).values.each do |attributes|
-      next if attributes["id"].present?
-
-      new_nested_object = send("build_#{nested_object_name.to_s.singularize}", attributes_without_destroy(attributes))
-      mark_for_destruction_if_necessary(new_nested_object, attributes)
-
-      existing_and_new_nested_objects << new_nested_object
-    end
-  end
-
   module ClassMethods
     
     attr_accessor :own_object_class
@@ -274,32 +176,12 @@ module ObjectAttorney
       end
     end
 
-    def accepts_nested_objects(*nested_objects_list)
-      self.instance_variable_set("@nested_objects", nested_objects_list)
-      self.send(:attr_accessor, *nested_objects_list.map { |attribute| "#{attribute}_attributes".to_sym })
-      define_nested_objects_getter_methods nested_objects_list
-    end
-
     def attr_white_list(*white_list)
       self.class.instance_variable_set("@white_list", white_list.map(&:to_s))
     end
 
     def attr_black_list(*black_list)
       self.class.instance_variable_set("@black_list", black_list.map(&:to_s))
-    end
-
-    def reflect_on_association(association)
-      nil
-    end
-
-    private #------------------------ private
-
-    def define_nested_objects_getter_methods(nested_objects_list)
-      nested_objects_list.each do |nested_object_name|
-        define_method(nested_object_name) do
-          nested_getter(nested_object_name)
-        end
-      end
     end
 
   end
---
+++ lib/object_attorney/nested_objects.rb
@@ -0,0 +1,138 @@
+module ObjectAttorney
+  module NestedObjects
+
+    def nested_objects
+      unless nested_objects_updated
+        @nested_objects = self.class.instance_variable_get("@nested_objects").map { |nested_object_sym| self.send(nested_object_sym) }.flatten
+        nested_objects_updated = true
+      end
+      
+      @nested_objects
+    end
+
+    def mark_for_destruction
+      @marked_for_destruction = true
+    end
+
+    def marked_for_destruction?
+      @marked_for_destruction
+    end
+
+    def mark_for_destruction_if_necessary(object, attributes)
+      return nil unless attributes.kind_of?(Hash)
+
+      _destroy = attributes["_destroy"] || attributes[:_destroy]
+
+      object.mark_for_destruction if ["true", "1"].include?(_destroy)
+    end
+
+    protected #--------------------------------------------------protected
+
+    def self.included(base)
+      base.extend(ClassMethods)
+
+      base.class_eval do
+        attr_accessor :nested_objects_updated
+        validate :validate_nested_objects
+      end
+
+      base.instance_variable_set("@nested_objects", [])
+    end
+
+    def save_or_destroy_nested_objects
+      result = nested_objects.map do |nested_object|
+        nested_object.marked_for_destruction? ? nested_object.destroy : nested_object.save!
+      end.all?
+
+      self.errors.add(:base, "Some errors where found while saving the nested objects.") unless result
+
+      result
+    end
+
+    def validate_nested_objects
+      #nested_objects.all?(&:valid?) #will not validate all nested_objects
+      return true if nested_objects.reject(&:marked_for_destruction?).map(&:valid?).all?
+      import_nested_objects_errors
+      false
+    end
+
+    def import_nested_objects_errors
+      self.class.instance_variable_get("@nested_objects").map do |nested_object_sym|
+        
+        [*self.send(nested_object_sym)].each do |nested_object|
+          nested_object.errors.full_messages.each { |message| self.errors.add(nested_object_sym, message) }
+        end
+
+      end
+    end
+
+    private #------------------------------ private
+
+    def nested_getter(nested_object_name)
+      nested_instance_variable = self.instance_variable_get("@#{nested_object_name}")
+
+      if nested_instance_variable.nil?
+        nested_instance_variable = get_existing_and_new_nested_objects(nested_object_name)
+        self.instance_variable_set("@#{nested_object_name}", nested_instance_variable)
+      end
+
+      nested_instance_variable
+    end
+
+    def get_existing_and_new_nested_objects(nested_object_name)
+      existing_and_new_nested_objects = []
+      
+      update_existing_nested_objects(existing_and_new_nested_objects, nested_object_name)
+      build_new_nested_objects(existing_and_new_nested_objects, nested_object_name)
+
+      existing_and_new_nested_objects
+    end
+
+    def update_existing_nested_objects(existing_and_new_nested_objects, nested_object_name)
+      (send("existing_#{nested_object_name}") || []).each do |existing_nested_object|
+        attributes = get_attributes_for_existing(nested_object_name, existing_nested_object)
+
+        mark_for_destruction_if_necessary(existing_nested_object, attributes)
+        existing_nested_object.assign_attributes(attributes_without_destroy(attributes))
+
+        existing_and_new_nested_objects << existing_nested_object
+      end
+    end
+
+    def build_new_nested_objects(existing_and_new_nested_objects, nested_object_name)
+      (send("#{nested_object_name}_attributes") || {}).values.each do |attributes|
+        next if attributes["id"].present?
+
+        new_nested_object = send("build_#{nested_object_name.to_s.singularize}", attributes_without_destroy(attributes))
+        mark_for_destruction_if_necessary(new_nested_object, attributes)
+
+        existing_and_new_nested_objects << new_nested_object
+      end
+    end
+
+    module ClassMethods
+
+      def accepts_nested_objects(*nested_objects_list)
+        self.instance_variable_set("@nested_objects", nested_objects_list)
+        self.send(:attr_accessor, *nested_objects_list.map { |attribute| "#{attribute}_attributes".to_sym })
+        define_nested_objects_getter_methods nested_objects_list
+      end
+
+      def reflect_on_association(association)
+        nil
+      end
+
+      private #------------------------ private
+
+      def define_nested_objects_getter_methods(nested_objects_list)
+        nested_objects_list.each do |nested_object_name|
+          define_method(nested_object_name) do
+            nested_getter(nested_object_name)
+          end
+        end
+      end
+
+    end
+
+  end
+end
