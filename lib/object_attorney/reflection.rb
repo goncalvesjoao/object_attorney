a28ea3e9a3a8d5b3bb4e38bc31e89366d66de550
João Gonçalves <goncalves.joao@gmail.com>
2014-01-01 22:59:48 +0000

    configurable single_name and plural_name

--- lib/object_attorney/reflection.rb
+++ lib/object_attorney/reflection.rb
@@ -1,15 +1,23 @@
 module ObjectAttorney
 
   class Reflection
-    attr_reader :name, :klass, :options, :single_name, :plural_name
+    attr_reader :name, :klass, :options
 
     def initialize(class_name, options)
       options = options.is_a?(Hash) ? options : { class_name: options }
 
-      @name, @options, @single_name, @plural_name = class_name, options, class_name.to_s.singularize, class_name.to_s.pluralize      
+      @name, @options = class_name, options
 
       @klass = options[:class_name] || klass_default(@name)
       @klass = @klass.constantize if @klass.is_a?(String)
+    end
+
+    def single_name
+      @single_name ||= options[:single_name] || name.to_s.singularize
+    end
+
+    def plural_name
+      @plural_name ||= options[:plural_name] || name.to_s.pluralize
     end
 
     private ################################# private
--- lib/object_attorney/version.rb
+++ lib/object_attorney/version.rb
@@ -1,3 +1,3 @@
 module ObjectAttorney
-  VERSION = "1.3.0"
+  VERSION = "1.3.1"
 end
