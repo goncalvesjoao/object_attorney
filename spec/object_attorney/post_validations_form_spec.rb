7a86f19e391b776473675268118126542c5d083d
João Gonçalves <goncalves.joao@gmail.com>
2014-01-10 12:26:26 +0000

    test fix

--- spec/object_attorney/post_validations_form_spec.rb
+++ spec/object_attorney/post_validations_form_spec.rb
@@ -2,18 +2,6 @@ require "spec_helper"
 
 describe PostValidationsForm do
 
-  it "asd", current: true do
-    post1 = Post.new
-    post1.valid?.should == true
-    
-    post1.singleton_class.validates_presence_of :title
-    # binding.pry
-    post1.valid?.should == false
-
-    post2 = Post.new
-    post2.valid?.should == true
-  end
-
   it "1. 'PostValidationsForm' becomes invalid if 'Post' has errors after the #submit method and incorporates its errors." do
     params = {
       post: {
@@ -31,23 +19,23 @@ describe PostValidationsForm do
     post_form.save
     
     post_form.should have(1).error_on(:title)
-    post_form.errors.size.should == 2
+    post_form.errors.size.should == 3
     post_form.comments.first.should have(1).error_on(:body)
 
     post_form.valid?.should == false
     post_form.should have(1).error_on(:title)
-    post_form.errors.size.should == 2
+    post_form.errors.size.should == 3
     post_form.comments.first.should have(1).error_on(:body)
 
     post_form.save
 
     post_form.should have(1).error_on(:title)
-    post_form.errors.size.should == 2
+    post_form.errors.size.should == 3
     post_form.comments.first.should have(1).error_on(:body)
 
     post_form.valid?.should == false
     post_form.should have(1).error_on(:title)
-    post_form.errors.size.should == 2
+    post_form.errors.size.should == 3
     post_form.comments.first.should have(1).error_on(:body)
   end
 
